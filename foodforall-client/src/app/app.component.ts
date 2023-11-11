7e511785f822b596c88733f92f294f53722f8396
nicugnm <nghergu17@gmail.com>
2023-11-11 15:33:27 +0200

    add nx and tailwind elements instead of angular material

--- foodforall-client/src/app/app.component.ts
+++ foodforall-client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Carousel,
+  Ripple,
+  initTE, } from 'tw-elements';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foodforall-client';
+
+  ngOnInit() {
+    initTE({ Carousel, Ripple });
+  }
 }
--- foodforall-client/tailwind.config.js
+++ foodforall-client/tailwind.config.js
@@ -1,10 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [require("tw-elements/dist/plugin")],
+  darkMode: "class",
+};
