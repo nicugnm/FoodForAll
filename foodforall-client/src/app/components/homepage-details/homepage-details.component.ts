d8fda61ad247dc476d1b1c83979913436b126376
nicugnm <nghergu17@gmail.com>
2023-11-13 18:37:00 +0200

    add all new files

---
+++ foodforall-client/src/app/components/homepage-details/homepage-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Flowbite} from "../../hacks";
+
+@Component({
+  selector: 'app-homepage-details',
+  templateUrl: './homepage-details.component.html',
+  styleUrls: ['./homepage-details.component.css']
+})
+@Flowbite()
+export class HomepageDetailsComponent {
+
+}
